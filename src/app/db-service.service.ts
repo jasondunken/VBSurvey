929447f3d1fafba19a62c91d662cb659c000c78a
jasondunken <jasondunken@yahoo.com>
2020-02-20 16:35:00 -0500

    updated urls to work with cloud.gov

--- src/app/db-service.service.ts
+++ src/app/db-service.service.ts
@@ -1,30 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
 
-import { Record } from './record';
+import { Record } from "./record";
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json"
   })
 };
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class DbServiceService {
-  private apiUrl = 'http://127.0.0.1:4000/api';
-  // private apiUrl = 'http://vbserver.apps.internal:3000/api';
-  private apiBatchUrl = 'http://127.0.0.1:4000/api/batch';
-  // private apiBatchUrl = 'http://vbserver.apps.internal:3000/api/batch';
+  // private apiUrl = 'http://127.0.0.1:4000/api';
+  private apiUrl = "https://vbsurvey.app.cloud.gov/api";
+  // private apiBatchUrl = 'http://127.0.0.1:4000/api/batch';
+  private apiBatchUrl = "https://vbsurvey.app.cloud.gov/api/batch";
 
   constructor(private http: HttpClient) {}
 
   getRecords(query): Observable<Record[]> {
     return this.http.get<Record[]>(this.apiUrl + query).pipe(
-      tap(_ => this.log('fetched records')),
-      catchError(this.handleError<Record[]>('getRecords', []))
+      tap(_ => this.log("fetched records")),
+      catchError(this.handleError<Record[]>("getRecords", []))
     );
   }
 
@@ -32,8 +32,10 @@ export class DbServiceService {
   addRecord(record: Record): Observable<Record> {
     const jsonRecord = JSON.stringify(record);
     return this.http.post<Record>(this.apiUrl, jsonRecord, httpOptions).pipe(
-      tap((newRecord: any) => this.log(`added new record to db: ${newRecord.vbserver}`)),
-      catchError(this.handleError<Record>('addRecord'))
+      tap((newRecord: any) =>
+        this.log(`added new record to db: ${newRecord.vbserver}`)
+      ),
+      catchError(this.handleError<Record>("addRecord"))
     );
   }
 
@@ -41,19 +43,19 @@ export class DbServiceService {
   batchLoadRecords(records: Record[]): Observable<any> {
     const jsonRecord = JSON.stringify(records);
     return this.http.post(this.apiBatchUrl, jsonRecord, httpOptions).pipe(
-      tap((newRecord: any) => this.log(`db: ${newRecord.vbserver}`)),
-      catchError(this.handleError<Record>('addRecord'))
+      tap((response: any) => this.log(`db: ${response.vbserver}`)),
+      catchError(this.handleError<Record>("addRecord"))
     );
   }
 
-  private handleError<T>(operation = 'operation', result?: T) {
+  private handleError<T>(operation = "operation", result?: T) {
     return (error: any): Observable<T> => {
       if (error.status === 0) {
-        alert('The server is currently unavailable, please try again later.');
+        alert("The server is currently unavailable, please try again later.");
       } else if (error.status === 400) {
-        alert('The server had an issue with your request: ' + error.status);
+        alert("The server had an issue with your request: " + error.status);
       } else {
-        alert('An error has been encountered, error.status: ' + error.status);
+        alert("An error has been encountered, error.status: " + error.status);
       }
       this.log(`${operation} failed: ${error.message}`);
       return of(result as T);
